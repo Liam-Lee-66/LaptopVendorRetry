bc0d78294acbae9647155ae233c4a646c3078889
Liam <liamsjlee@gmail.com>
2023-02-16 15:14:22 -0600

    add test version brand controller & view

---
+++ LaptopVendorRetry/Controllers/BrandsController.cs
@@ -0,0 +1,14 @@
+﻿using LaptopVendorRetry.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaptopVendorRetry.Controllers
+{
+    public class BrandsController : Controller
+    {
+        public IActionResult Index()
+        {
+            using LaptopVendorContext context = new LaptopVendorContext();
+            return View(context.Brands.ToList());
+        }
+    }
+}
